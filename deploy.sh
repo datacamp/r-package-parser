346dc6d541a0c793ebb0a8e71a1d8e3b68b99575
Filip Schouwenaars <filip@datacamp.com>
2017-02-14 19:40:33 +0100

    add jenkins

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-BUILD_NUMBER=$(git rev-parse --short HEAD)
-docker build -t dockerhub.datacamp.com:443/rdoc-ecs-worker:$BUILD_NUMBER .
-docker push dockerhub.datacamp.com:443/rdoc-ecs-worker:$BUILD_NUMBER
---
+++ jenkins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+TAG="dockerhub.datacamp.com:443/rdoc-ecs-worker:$BUILD_NUMBER
+docker build -t $TAG .
+docker login --username="$1" --password="$2" dockerhub.datacamp.com:443
+docker push $TAG
