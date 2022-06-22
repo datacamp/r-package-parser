88c75c493a38d7d26ef27324008b2559f848ec8d
Filip Schouwenaars <filipsch@users.noreply.github.com>
2022-06-22 09:38:03 +0200

    chore: delete jenkins.sh file as it's no longer used

--- jenkins.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TAG=dockerhub.datacamp.com:443/rdoc-ecs-worker:$BUILD_NUMBER
-docker build -t $TAG .
-docker login --username="$1" --password="$2" dockerhub.datacamp.com:443
-docker push $TAG
