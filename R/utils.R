5db73a9dbc612a478031ca4236005994d64e1db4
Filip Schouwenaars <filipsch@users.noreply.github.com>
2022-06-23 09:02:55 +0200

    refactor(untar): refactor how tar is downloaded and unpacked
    - Use withr::with_tempdir, which is more elegant on.exit logic
    - Add ability to point to local tar file (handy when coding on the bus!)
    - Reorder some files in R/processing.R

--- R/utils.R
+++ R/utils.R
@@ -3,21 +3,20 @@ delete_files <- function() {
   unlink("packages", recursive = TRUE)
 }
 
-download_and_unpack <- function(pkg_url, pkg_name) {
-  message("Downloading and unpacking tarball ...")
+unpack_pkg_tar <- function(path) {
 
-  tmp_dir <- tempfile(pattern = "package_")
-  dir.create(tmp_dir)
-  tar_path <- file.path(tmp_dir, paste0(pkg_name, ".tar.gz"))
-  options(timeout = 30)
-  tryCatch(download.file(pkg_url, tar_path, quiet = TRUE),
-           error = function(e) simpleError('archive not found'))
+  if(file.exists(path)) {
+    local_path <- path
+  } else {
+    message("Downloading tarball ", path, " ...")
+    local_path <- basename(path)
+    options(timeout = 30)
+    tryCatch(download.file(path, local_path, quiet = TRUE),
+             error = function(e) simpleError('archive not found'))
+  }
 
-  untar_dir <- file.path(tmp_dir, pkg_name)
-  untar(tar_path, exdir = untar_dir)
-  # file.remove(tar_path)
-
-  pkg_folder <- list.dirs(untar_dir, recursive = FALSE)
-
+  message("Unpacking tarball ...")
+  untar(local_path)
+  pkg_folder <- list.dirs(recursive = FALSE)
   return(pkg_folder)
 }
